f8c2ffaec2c559a646b4274e29ecc3c37fc3d988
Richard Ranft <vegassparkyrich@gmail.com>
2022-04-19 14:08:11 -0700

    work on Raathan name generation

--- NameGen/Scripts/Raathan.lua
+++ NameGen/Scripts/Raathan.lua
@@ -4,31 +4,24 @@ require "utilities"
 -- general name generation
 Raathan = {};
 
-Raathan["Syllables"] = {};
-Raathan["ClanNames"] = {};
-Raathan["ClanParts"] = {};
+Raathan["FirstSyllables"] = {};
+Raathan["LastSyllables"] = {};
 
 function Raathan.GetName(namecount)
 	if namecount == nil then
 		namecount = 1;
 		--csLog.LogInfo("Raathan.lua", "namecount is nil, setting to 1");
 	end
-	if utilities.tablelength(Raathan.Syllables) < 1 then
+	if utilities.tablelength(Raathan.FirstSyllables) < 1 or utilities.tablelength(Raathan.LastSyllables) < 1 then
 		Raathan.loadSyllables();
-	end
-	if utilities.tablelength(Raathan.ClanNames) < 1 then
-		Raathan.loadClanNames();
-	end
-	if utilities.tablelength(Raathan.ClanParts) < 1 then
-		Raathan.loadClanParts();
 	end
 	local names = {};
 	for i = 1, namecount do
 		--csLog.LogInfo("Raathan.lua", "GetName() generating " .. tostring(i));
 		local first = Raathan.GetPersonalName();
-		first = first:gsub("^%l", string.upper); -- uppercase first character
+		first = first:gsub("^%l", string.upper);  --uppercase first character
 		local last = Raathan.GetClanName();
-		last = last:gsub("^%l", string.upper); -- uppercase first character
+		last = last:gsub("^%l", string.upper);  --uppercase first character
 		local name = tostring(first) .. " " .. tostring(last);
 		table.insert(names, name);
 	end
@@ -38,83 +31,78 @@ end
 
 function Raathan.GetPersonalName()
 	local name = "";
-	local count = utilities.tablelength(Raathan.Syllables);
-	local scount = math.random(2, 3);
+	local count = tonumber(utilities.tablelength(Raathan.FirstSyllables));
+	local limit = Raathan.FirstSyllables[count - 1].Count;
+	local scount = math.random(2, 4);
 	for i = 1, scount do
-		name = name .. Raathan.Syllables[math.random(1, count)];
+		local value = math.random(1, limit);
+		local part = "";
+		for i = 1, count do
+			if Raathan.FirstSyllables[i].Count <= value then
+				part = Raathan.FirstSyllables[i].Syllable;
+			end
+		end
+		name = name .. part;
 	end
 	return name;
 end
 
 function Raathan.GetClanName()
-	local count1 = utilities.tablelength(Raathan.ClanNames[1]);
-	local count2 = utilities.tablelength(Raathan.ClanNames[2]);
-	local clan1 = Raathan.ClanNames[1][math.random(1, count1)];
-	local clan2 = Raathan.ClanNames[2][math.random(1, count2)];
-	if string.find(clan1, "%[") ~= nil then
-		clan1 = Raathan.ClanParts[clan1][math.random(1, utilities.tablelength(Raathan.ClanParts[clan1]))];
-		while string.find(clan2, "%[") ~= nil do
-			clan2 = Raathan.ClanNames[2][math.random(1, count2)];
+	local name = "";
+	local count = tonumber(utilities.tablelength(Raathan.LastSyllables));
+	local limit = Raathan.LastSyllables[count - 1].Count;
+	local scount = math.random(3, 5);
+	for i = 1, scount do
+		local value = math.random(1, limit);
+		local part = "";
+		for i = 1, count do
+			if Raathan.LastSyllables[i].Count <= value then
+				part = Raathan.LastSyllables[i].Syllable;
+			end
 		end
-	elseif string.find(clan2, "%[") ~= nil then
-		clan2 = Raathan.ClanParts[clan2][math.random(1, utilities.tablelength(Raathan.ClanParts[clan2]))];
-		while string.find(clan1, "%[") ~= nil do
-			clan1 = Raathan.ClanNames[1][math.random(1, count1)];
-		end
+		name = name .. part;
 	end
-	local clan = clan1 .. clan2;
-	return clan;
+	return name;
 end
 
 function Raathan.loadSyllables()
-	local handle, msg, err = io.open("data\\CSV_Illuminan_Syllables.csv");
+	local handle, msg, err = io.open("Data\\CSV_Database_of_First_Names.xml.csv");
 	math.randomseed(tonumber(os.time()));
 	if handle == nil then
-		csLog.LogError("Raathan.lua", "Unable to open data\\CSV_Illuminan_Syllables.csv");
+		csLog.LogError("Raathan.lua", "Unable to open data\\CSV_Database_of_First_Names.xml.csv");
 		csLog.LogError(tostring(err) .. ": ".. tostring(msg));
 	else
 		local buffer = handle:read();
 		handle:close();
-		Raathan.Syllables = utilities.split(buffer, '[^,]+');
+		local entries = utilities.split(buffer, '[^,]+');
+		local count = 0;
+		for i = 1, utilities.tablelength(entries) do
+			local entry = {};
+			local parts = utilities.split(entries[i], '[^:]+');
+			entry["Syllable"] = parts[1];
+			count = count + tonumber(parts[2]);
+			entry["Count"] = count;
+			table.insert(Raathan.FirstSyllables, entry);
+		end
 	end
-end
-
-function Raathan.loadClanNames()
-	local handle, msg, err = io.open("data\\CSV_Illuminan_Clans.csv");
+	handle, msg, err = io.open("data\\CSV_Database_of_Last_Names.xml.csv");
+	math.randomseed(tonumber(os.time()));
 	if handle == nil then
-		csLog.LogError("Raathan.lua", "Unable to open data\\CSV_Illuminan_Clans.csv");
+		csLog.LogError("Raathan.lua", "Unable to open data\\CSV_Database_of_Last_Names.xml.csv");
 		csLog.LogError(tostring(err) .. ": ".. tostring(msg));
 	else
-		for line in handle:lines() do
-			local entries = utilities.split(line, '[^,]+');
-			table.insert(Raathan.ClanNames, entries);
-		end
+		local buffer = handle:read();
 		handle:close();
-	end
-end
-
-function Raathan.loadClanParts()
-	--csLog.LogInfo("Raathan.lua", "loadClanParts()");
-	local handle, msg, err = io.open("data\\CSV_Illuminan_Clan_Parts.csv");
-	if handle == nil then
-		csLog.LogError("Raathan.lua", "Unable to open data\\CSV_Illuminan_Clan_Parts.csv");
-		csLog.LogError(tostring(err) .. ": ".. tostring(msg));
-	else
-		local currTable = "";
-		for line in handle:lines() do
-			if string.find(line, "%[") ~= nil then
-				currTable = line;
-				Raathan.ClanParts[currTable] = {};
-				--csLog.LogInfo("Raathan.lua", "loadClanParts() - Adding table " .. currTable);
-			else
-				local entries = utilities.split(line, '[^,]+');
-				for entry in utilities.list_iter(entries) do
-					--csLog.LogInfo("Raathan.lua", "loadClanParts() - Adding " .. tostring(entry) .. " to " .. tostring(currTable));
-					table.insert(Raathan.ClanParts[currTable], entry);
-				end
-			end
+		local entries = utilities.split(buffer, '[^,]+');
+		local count = 0;
+		for i = 1, utilities.tablelength(entries) do
+			local entry = {};
+			local parts = utilities.split(entries[i], '[^:]+');
+			entry["Syllable"] = parts[1];
+			count = count + tonumber(parts[2]);
+			entry["Count"] = count;
+			table.insert(Raathan.LastSyllables, entry);
 		end
-		handle:close();
 	end
 end
 
